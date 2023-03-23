828dca88ccccf7d29fd1c000016a0f29567b6642
pallavi149 <5811@holbertonstudents.com>
2023-03-23 05:36:46 -0700

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""This module defines a class to manage DBstorage"""
+import MySQLdb
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+ModelDict = {
+    'User': User,
+    'Place': Place,
+    'State': State,
+    'City': City,
+    'Amenity': Amenity,
+    'Review': Review
+}
+
+
+class DBStorage:
+    """This class manages storage of hbnb models"""
+    def __init__(self):
+        self.__engine = "None"
+        self.__session = "None"
+
+        # Retrieve the value of the "HBNB_MYSQL_USER" environment variable
+        user = os.environ.get("HBNB_MYSQL_USER")
+        password = os.environ.get("HBNB_MYSQL_PWD")
+        # Retrieve the value of the "HBNB_MYSQL_HOST" environment variable with
+        # a default value
+        host = os.environ.get("HBNB_MYSQL_HOST", "localhost")
+        database = os.environ.get("HBNB_MYSQL_DB")
+
+        db_url = (f"mysql+mysqldb://{user}:{password}@{host}/{database}")
+        self.__engine = create_engine(db_url, echo=True, pool_pre_ping=True)
+
+        if os.getenv("HBNB_ENV") == "test":
+            print("test env: dropping all table")
+            Base.metadata.drop_all(bind=self.__engine)
+
+        # scoped_session to ensure thread_safety when creates a new database
+        # session
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+            expire_on_commit=False))
+
+    def all(self, cls=None):
+        dictionary = {}
+        if cls:
+            modelClass = ModelDict[cls]
+            self.readAllAsDict(cls, modelClass, dictionary)
+        else:
+            for className, modelClass in ModelDict.items():
+                self.readAllAsDict(className, modelClass, dictionary)
+        return dictionary
+
+    def readAllAsDict(self, cls, modelClass, dictionary):
+        if modelClass:
+            records = self.__session.query(modelClass).all()
+            for row in records:
+                dictionary["{}.{}".format(cls, row.id)] = row
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete()
+        else:
+            self.__session.query(self.__class__).delete()
+        self.__session.commit()
+
+    def reload(self):
+        if self.__session:
+            self.__session.close()
+        # Ensure all tables are created in the database
+        Base.metadata.create_all(self.__engine)
+        # scoped_session to ensure thread_safety when creates a new database
+        # session
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+            expire_on_commit=False))
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+            City,
+            backref="state",
+            cascade="all, delete"
+        )
