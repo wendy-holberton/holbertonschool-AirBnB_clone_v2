2a4e9daa85c7e03d8bc6e06b6998995261da4fa0
pallavi149 <5811@holbertonstudents.com>
2023-03-23 05:58:23 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+from os import getenv
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Class representing amenities"""
+    __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place',
+                                   secondary=place_amenity,
+                                   back_populates='amenities'
+                                   )
+    else:
+        name = ""
