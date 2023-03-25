89aa058dcdbf6a9882dc668459d96c93b70928e7
wendy <5703@holbertonstudents.com>
2023-03-24 21:06:54 -0700

    test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for testing amenity """
+from tests.test_models.test_base_model import test_basemodel
+from models.console import Console
+
+
+class test_Console(test_basemodel):
+    """ Class to test the Console method """
+
+    def __init__(self, *args, **kwargs):
+        """ Define __init__ function """
+        super().__init__(*args, **kwargs)
+        self.name = "Console"
+        self.value = Console
