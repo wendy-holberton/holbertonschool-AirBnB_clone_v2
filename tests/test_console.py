e124fea4ff248713aabd4954019f8a544245fb77
wendy <5703@holbertonstudents.com>
2023-03-24 21:39:41 -0700

    tests/test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Module for testing amenity """
-from tests.test_models.test_base_model import test_basemodel
-from console import Console
+import unittest
 
-class test_Console(test_basemodel):
+class test_Console(unittest.TestCase):
     """ Class to test the Console method """
 
     def __init__(self, *args, **kwargs):
         """ Define __init__ function """
         super().__init__(*args, **kwargs)
         self.name = "Console"
-        self.value = Console
