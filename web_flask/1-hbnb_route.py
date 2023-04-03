e05b1697a53cad00614083a6cd717c981ca1886a
Wendy <you@example.com>
2023-04-03 04:31:33 -0700

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,9 +8,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", methods=['GET'], strict_slashes=False)
 def hbnb():
